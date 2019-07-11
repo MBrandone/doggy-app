b5ab87fc24fff138280ff06328640482e8d60dca
Brandone Martins <brandone.martins@gmail.com>
2019-07-11 18:00:37 +0200

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import doggyLogo from "./doggy-logo.png"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function random(array) {
+  return array[Math.floor(Math.random() * array.length)]
 }
 
-export default App;
+const animations = [
+  "spin-animation",
+  "bounce-animation",
+  "shake-animation",
+  "shrink-animation"
+]
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      citations: [
+        "Pépite !",
+        "Senior Manager de mon cul !",
+        "J'en veux Jooooooooooe !",
+        "Et ce soir ... on chope !",
+        "Tu sais pas qui chui !",
+        "Y a 5 places dans une Mégane.",
+        "Missile !",
+        "Oh l'enfer !",
+        "Ah !"
+      ],
+      currentCitation: ""
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.changeAnim = this.changeAnim.bind(this)
+  }
+
+  handleClick() {
+    const state = this.state
+    state.currentCitation = random(state.citations)
+    this.setState(state)
+  }
+
+  changeAnim() {
+    const image = document.getElementsByClassName("App-logo");
+    image[0].classList.remove("spin-animation", "bounce-animation", "shake-animation", "shrink-animation");
+    image[0].classList.add(random(animations));
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <button onClick={this.changeAnim}>
+            <img src={doggyLogo} className="App-logo" alt="logo"/>
+          </button>
+          <div className="citation">
+            {random(this.state.citations)}
+          </div>
+          <button onClick={this.handleClick}>
+            Clique moi !
+          </button>
+        </div>
+      </div>
+    )
+
+  }
+}
+
+export default App
