a9ba63b2ce0f1519613f0a6636e4e33a9a76ca48
Brandone Martins <brm@octo.com>
2019-12-24 18:50:28 +0100

    Ajout d'une animation sur le logo de la première page

--- web/src/DoggyQuiz/composants/LogoDoggyQuizAvecFruits/index.js
+++ web/src/DoggyQuiz/composants/LogoDoggyQuizAvecFruits/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import "./logoDoggyQuizAvecFruits.scss"
-import "../../../styles/animations/tourne-en-rond.scss"
+import "../../../styles/animations/index.scss"
 
 const LogoDoggyQuizAvecFruits = _ => (
-  <div className="logo-doggy-quiz-avec-fruits tourne-en-rond">
+  <div className="logo-doggy-quiz-avec-fruits apparition-tournante">
     <img src="/images/logo-doggy-quiz.svg" alt="Le logo du doggy quiz" className="doggy-quiz"/>
     <img src="/images/aubergine.svg" alt="une aubergine" className="aubergine"/>
     <img src="/images/peche.svg" alt="une peche" className="peche"/>
